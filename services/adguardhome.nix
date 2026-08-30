92d9929c498426c7715bd8f5e116434f97633f24
kaz <kizo.aria@protonmail.com>
2026-08-30 03:00:47 +0100

    make adguard fully declarative

--- services/adguardhome.nix
+++ services/adguardhome.nix
@@ -2,11 +2,24 @@
   services.adguardhome = {
     enable = true;
     port = 3000;
+    mutableSettings = false;
     settings = {
-      dns.upstream_dns = [
-        "1.1.1.1"
-        "9.9.9.9"
+      users = [
+        {
+          name = "kaz";
+          password = "$2a$10$x2u76NroVzca7zS/c/vP3ODEQe8YiupIjWmBTAd1EiZ.aYEjPTEim";
+        }
       ];
+      dns = {
+        upstream_dns = [
+          "1.1.1.1"
+          "9.9.9.9"
+        ];
+        bootstrap_dns = [
+          "1.1.1.1"
+          "9.9.9.9"
+        ];
+      };
       querylog.interval = "90d";
       statistics.interval = "90d";
       user_rules = [ "@@||whatismyipaddress.com^$important" ];
@@ -33,11 +46,6 @@
         }
         {
           domain = "immich.kaaz.top";
-          answer = "100.64.0.1";
-          enabled = true;
-        }
-        {
-          domain = "vault.kaaz.top";
           answer = "100.64.0.1";
           enabled = true;
         }
@@ -72,6 +80,102 @@
           enabled = true;
         }
       ];
+      filters = [
+        {
+          enabled = true;
+          url = "https://adguardteam.github.io/HostlistsRegistry/assets/filter_1.txt";
+          name = "AdGuard DNS filter";
+          id = 1;
+        }
+        {
+          enabled = true;
+          url = "https://adguardteam.github.io/HostlistsRegistry/assets/filter_2.txt";
+          name = "AdAway Default Blocklist";
+          id = 2;
+        }
+        {
+          enabled = false;
+          url = "https://adguardteam.github.io/HostlistsRegistry/assets/filter_70.txt";
+          name = "1Hosts (Xtra)";
+          id = 1786309258;
+        }
+        {
+          enabled = true;
+          url = "https://adguardteam.github.io/HostlistsRegistry/assets/filter_27.txt";
+          name = "OISD Blocklist Big";
+          id = 1786309259;
+        }
+        {
+          enabled = true;
+          url = "https://adguardteam.github.io/HostlistsRegistry/assets/filter_30.txt";
+          name = "Phishing URL Blocklist (PhishTank and OpenPhish)";
+          id = 1786309260;
+        }
+        {
+          enabled = true;
+          url = "https://adguardteam.github.io/HostlistsRegistry/assets/filter_10.txt";
+          name = "Scam Blocklist by DurableNapkin";
+          id = 1786309261;
+        }
+        {
+          enabled = true;
+          url = "https://adguardteam.github.io/HostlistsRegistry/assets/filter_42.txt";
+          name = "ShadowWhisperer's Malware List";
+          id = 1786309262;
+        }
+        {
+          enabled = true;
+          url = "https://adguardteam.github.io/HostlistsRegistry/assets/filter_31.txt";
+          name = "Stalkerware Indicators List";
+          id = 1786309263;
+        }
+        {
+          enabled = true;
+          url = "https://adguardteam.github.io/HostlistsRegistry/assets/filter_9.txt";
+          name = "The Big List of Hacked Malware Web Sites";
+          id = 1786309264;
+        }
+        {
+          enabled = true;
+          url = "https://adguardteam.github.io/HostlistsRegistry/assets/filter_50.txt";
+          name = "uBlock₀ filters – Badware risks";
+          id = 1786309265;
+        }
+        {
+          enabled = true;
+          url = "https://adguardteam.github.io/HostlistsRegistry/assets/filter_11.txt";
+          name = "Malicious URL Blocklist (URLHaus)";
+          id = 1786309266;
+        }
+        {
+          enabled = true;
+          url = "https://adguardteam.github.io/HostlistsRegistry/assets/filter_59.txt";
+          name = "AdGuard DNS Popup Hosts filter";
+          id = 1786309267;
+        }
+        {
+          enabled = true;
+          url = "https://adguardteam.github.io/HostlistsRegistry/assets/filter_69.txt";
+          name = "ShadowWhisperer Tracking List";
+          id = 1786309268;
+        }
+        {
+          enabled = true;
+          url = "https://adguardteam.github.io/HostlistsRegistry/assets/filter_33.txt";
+          name = "Steven Black's List";
+          id = 1786309269;
+        }
+      ];
+      log = {
+        enabled = true;
+        file = "";
+        max_backups = 0;
+        max_size = 100;
+        max_age = 3;
+        compress = false;
+        local_time = false;
+        verbose = false;
+      };
     };
   };
 
