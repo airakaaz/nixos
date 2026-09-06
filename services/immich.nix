112a8a67b0d5428980b92b78f055e17a46b67fdc
kaz <kizo.aria@protonmail.com>
2026-09-06 20:59:36 +0100

    add fail2ban jails for immich and jellyfin

--- services/immich.nix
+++ services/immich.nix
@@ -14,4 +14,20 @@
   '';
 
   services.cloudflare-ddns.ip4Domains = [ "immich.kaaz.top" ];
+
+  services.fail2ban.jails.immich = {
+    filter = {
+      Definition = {
+        failregex = "^.*Failed login attempt for user .* from ip address <HOST>.*$";
+        journalmatch = "_SYSTEMD_UNIT=immich-server.service";
+      };
+    };
+    settings = {
+      port = "http,https";
+      maxretry = 5;
+      findtime = 15 * 60;
+      bantime = 1 * 60 * 60;
+    };
+  };
+
 }
--- services/jellyfin.nix
+++ services/jellyfin.nix
@@ -12,4 +12,20 @@
   '';
 
   services.cloudflare-ddns.ip4Domains = [ "watch.kaaz.top" ];
+
+  services.fail2ban.jails.jellyfin = {
+    filter = {
+      Definition = {
+        failregex = "^.*Authentication request for .* has been denied \(IP: [\"]?<HOST>[\"]?\).*";
+        journalmatch = "_SYSTEMD_UNIT=jellyfin.service";
+      };
+    };
+    settings = {
+      port = "http,https";
+      maxretry = 5;
+      findtime = 15 * 60;
+      bantime = 1 * 60 * 60;
+    };
+  };
+
 }
